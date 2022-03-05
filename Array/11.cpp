bc39ad9f7c0130e2ce8381faca70f48c22696097
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-03-05 19:20:02 +0900

    Container With Most Water

---
+++ Array/11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+    }
+};
+
+int main(){
+    vector<int> temp = {
+        1,8,6,2,5,4,8,3,7
+    };
+    Solution s;
+    cout<<s.maxArea(temp);
+    return 0;
+}//Kadane Algorithm O(N)
