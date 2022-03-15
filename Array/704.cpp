607787e1fa3c66b62a2782ca50df06feff3ff87f
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-03-15 16:19:50 +0900

    Binary Search

--- Array/704.cpp
+++ Array/704.cpp
@@ -5,12 +5,16 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();++i){
-            if(target == nums[i]) return i;
+        int start = 0,end = nums.size() - 1;
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] > target) end = mid - 1;
+            else start = mid + 1;
         }
         return -1;
     }
-};
+};//Binary Search O(log(N))
 
 int main(){
     
