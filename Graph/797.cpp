1190bb67d9426b3355dbc4b3d321750ffbbe17d5
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-03-25 06:08:14 +0900

    All Paths From Source to Target

--- Graph/797.cpp
+++ Graph/797.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class Solution {
+class Solution {//Worst Case O(N^2)
 public:
-    vector<vector<int> > answer;
-    vector<int> temp;
-    void Search(int n,int N,vector<vector<int>>& arr){
-        if(n == N){
+    void Search(int n,int N,vector<vector<int>>& arr,vector<vector<int> >& answer,
+    vector<int>& temp){
+        if(n == N){//condition
             answer.push_back(temp);
             return ;
         }
         for(int i=0;i<arr[n].size();++i){
             temp.push_back(arr[n][i]);
-            Search(arr[n][i],N,arr);
+            Search(arr[n][i],N,arr,answer,temp);
             temp.pop_back();
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int> > answer;
+        vector<int> temp;
         int N = graph.size();
         temp.push_back(0);
-        Search(0,N-1,graph);
+        Search(0,N-1,graph,answer,temp);
         return answer;
     }
 };
