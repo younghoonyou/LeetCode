3e94a21f131aafd12debd25eedf67f4636277a87
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-04-08 08:22:32 +0900

    Count Primes

--- Math/204.cpp
+++ Math/204.cpp
@@ -7,16 +7,16 @@ public:
     int countPrimes(int n) {
         if(n<3) return 0;
         vector<bool> arr(n+1,false);
-        vector<int> primes;
+        int cnt = 0;
         for(int i=2;i<n;++i){
             if(arr[i]) continue;
-            primes.push_back(i);
+            cnt++;
             for(int j=i*1;j<=n;j+=i){
                 if(arr[j]) continue;
                 arr[j] = true;
             }
         }
-        return primes.size();
+        return cnt;
     }
 };
 
