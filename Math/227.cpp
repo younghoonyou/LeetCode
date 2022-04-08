3516e6591c4d526daab46ca542f44164fecfe6ba
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-04-09 06:58:19 +0900

    Basic Calculator II

---
+++ Math/227.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    long Cal(long a,long b,char ch){
+        if(ch == '+') return a+b;
+        else if(ch == '*') return a*b;
+        else return a/b;
+    }
+    int calculate(string s) {
+        stack<long> num;
+        stack<char> oper;
+        char tmp = ' ';
+        for(int i=0;i<s.length();++i){
+            if(s[i] == ' ') continue;
+            if(isdigit(s[i])){
+                if(isdigit(tmp)){
+                    long a = num.top();
+                    num.pop();
+                    a = 10*a + s[i] - '0';
+                    num.push(a);
+                }
+                else num.push(s[i] - '0');
+            }
+            else{
+                if(s[i] == '*' || s[i] == '/' || s[i] == '-'){
+                    string temp = "";
+                    int j = i+1;
+                    while(s[j] == ' ') ++j;
+                    while(isdigit(s[j])){
+                        temp+=s[j];
+                        ++j;
+                    }
+                    long tmp = stoi(temp);
+                    if(s[i] == '-'){
+                        std::cout<<-tmp;
+                        oper.push('+');
+                        num.push(-tmp);
+                        i = j - 1;
+                        continue;
+                    }
+                    long a = num.top();
+                    num.pop();
+                    tmp = Cal(a,tmp,s[i]);
+                    num.push(tmp);                    
+                    if(j == s.length() - 1) break;
+                    i = j - 1;
+                }
+                else oper.push(s[i]);
+            }
+            tmp = s[i];
+        }
+        while(!oper.empty()){
+            long b = num.top();
+            num.pop();
+            long a = num.top();
+            num.pop();
+            char ch = oper.top();
+            oper.pop();
+            long tmp = Cal(a,b,ch);
+            num.push(tmp);
+        }
+        return num.top();
+    }
+};
+
+int main(){}
