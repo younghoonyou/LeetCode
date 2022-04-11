d54d6cfdb7282f5e8b672c5ad9bf1064d83a6e1c
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-04-12 05:09:55 +0900

    Odd Even Linked List

---
+++ Linked_List/328.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode *odd = head;
+        ListNode *even_move = head->next;
+        ListNode *even = even_move;
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = even_move;
+        return head;
+    }
+};
+
+int main(){}
