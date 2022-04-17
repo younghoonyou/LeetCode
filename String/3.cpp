d4a110dcd1e4bd217e49ca3b5c39e042f43ea69f
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-04-18 06:54:04 +0900

    Longest Substring Without Repeating Characters

--- String/3.cpp
+++ String/3.cpp
@@ -1,39 +1,50 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
+class Solution//O(N^2) Solution But Fast than O(N) because of DP
+{
 public:
-    int lengthOfLongestSubstring(string s){
-        if(s.length() == 0) return 0;
-        if(s.length() == 1) return 1;
-        bool alpha[26] = {false,};//중복 확인
-        int answer = 1;
-        int arr[26];
-        for(int i=0;i<s.length()-1;++i){
-            if(!isalpha(s[i])) continue;
-            int index = 0;
-            int temp = 1;
-            alpha[s[i]-'a'] = true;//확인 표시
-            for(int j=i+1;j<s.length();++j){
-                if(!isalpha(s[j])) continue;
-                if(alpha[s[j]-'a']) break;
-                alpha[s[j]-'a'] = true;
-                arr[index++] = s[j]-'a';
-                temp++;
-                if(answer < temp) answer = temp;
+    int lengthOfLongestSubstring(string s)
+    {
+        int ans = 0;
+        bool arr[95];
+        for (int i = 0; i < s.length(); ++i)
+        {
+            memset(arr, false, sizeof(arr));
+            int cnt = 0;
+            for (int j = i; j < s.length(); ++j)
+            {
+                if (arr[(int)s[j] - 32])
+                    break;
+                arr[(int)s[j] - 32] = true;
+                cnt++;
             }
-            for(int t=0;t<index;++t) arr[arr[t]] = 0,alpha[arr[t]] = false;//원상 복구
-            alpha[s[i]-'a'] = false;//확인 제거
+            ans = max(ans, cnt);
         }
-        return answer;
+        return ans;
     }
 };
 
-int main(){
-    Solution s;
-    string str;
-    cin>>str;
-    int ans = s.lengthOfLongestSubstring(str);
-    cout<<ans;
-    return 0;
-}
+class Solution//O(N)
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char, bool> map;
+        int i, j, n = s.length(), ans;
+        i = j = ans = 0;
+        while (i < n && j < n)
+        {
+            if (map[s[j]])
+                map.erase(s[i++]);
+            else
+            {
+                map[s[j++]] = true;
+                ans = max(ans, j - i);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {}
