5c275b01a6ef8f272e281fb041833fad4775455c
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-04-17 18:40:46 +0900

    Trapping Rain Water

---
+++ Two_Pointer/42.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+class Solution{
+public:
+    int trap(vector<int> &height){
+        int start,end;
+        int sum = 0;
+        start = end = 0;
+        while(height[start]) start++,end++;
+
+        while (){
+        }
+        return sum;
+    }
+};
+
+int main() {}
