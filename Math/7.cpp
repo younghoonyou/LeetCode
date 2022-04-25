f063e51bb3a771a477a9d9ea424e58cc09505a96
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-04-25 17:04:39 +0900

    Reverse Integer

---
+++ Math/7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+class Solution {
+public:
+    int reverse(int x) {
+       long ans = 0;
+        while(x){
+            ans=(ans*10) + x%10;//increase 
+            x/=10;
+        }
+        return (ans < INT_MIN || ans > INT_MAX) ? 0:ans;
+    }
+};
+
+int main() {}
