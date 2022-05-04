011b3e08150932441efe1839d225c0d03261480f
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-05-05 07:10:33 +0900

    Two Sum

--- Array/1.cpp
+++ Array/1.cpp
@@ -2,20 +2,15 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-class Solution {
+class Solution{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> temp;
-    for(int i=0;i<nums.size()-1;++i){
-        for(int j=i+1;j<=nums.size()-1;++j){
-            int total = nums[i] + nums[j];
-            if(total==target){
-                temp.push_back(i);
-                temp.push_back(j);
-            }
+    vector<int> twoSum(vector<int> &nums, int target){//O(N)
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); ++i){
+            if (hash.find(target - nums[i]) != hash.end()) return {hash[target - nums[i]], i};
+            hash[nums[i]] = i;
         }
-    }
-        return temp;
+        return {};
     }
 };
 
