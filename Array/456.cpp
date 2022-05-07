d547729b15c939b552c3e0085005459ad3456d63
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-05-08 07:01:10 +0900

    132 Pattern

--- Array/456.cpp
+++ Array/456.cpp
@@ -6,15 +6,16 @@ using namespace std;
 class Solution{
 public:
     bool find132pattern(vector<int> &nums){
+        //condition (1) S1<S2 , (2) S1<S3 , (3) S3<S2
         int n = nums.size();
-        vector<int> mins(n, 0);
-        stack<int> s;
+        vector<int> mins(n, 0);//S1
+        stack<int> s;//S2
         mins[0] = nums[0];
         for (int i = 1; i < n; ++i) mins[i] = min(nums[i], mins[i - 1]);
         for (int j = n - 1; j > 0; j--){
-            if (nums[j] > mins[j]){
-                while (s.size() > 0 && s.top() <= mins[j]) s.pop();
-                if (s.size() > 0 && s.top() < nums[j]) return true;
+            if (nums[j] > mins[j]){//condition 2
+                while (s.size() > 0 && s.top() <= mins[j]) s.pop();//To meet condition 3
+                if (s.size() > 0 && s.top() < nums[j]) return true;//condition 1
                 s.push(nums[j]);
             }
         }
