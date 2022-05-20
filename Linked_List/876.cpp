fc3f2fcf23003871aa94e769b87c3b8d34741bee
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-05-20 16:04:07 +0900

    Middle of the Linked List

---
+++ Linked_List/876.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution{
+public:
+    ListNode *middleNode(ListNode *head){
+        if (!head || !head->next) return head;
+        ListNode *slow, *fast;
+        slow = fast = head;
+        while (fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+int main() {}
