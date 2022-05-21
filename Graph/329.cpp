b65fb4379c4f52c1a955c3574a05446ad09d39c3
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-05-21 13:24:02 +0900

    Longest Increasing Path in a Matrix

--- Graph/329.cpp
+++ Graph/329.cpp
@@ -5,47 +5,29 @@ using namespace std;
 
 class Solution{
 public:
-    int N;
-    int M;
-    int DFS(vector<vector<int>> &arr, vector<vector<bool>> &check, int x, int y){
-        if() return 1;
-        DFS(arr,check,x+1,y);
-        DFS(arr,check,x,y+1);
-        DFS(arr,check,x-1,y);
-        DFS(arr,check,x,y-1);
-
-    }
-    int BFS(vector<vector<int>>& arr,vector<vector<bool>>&check,int x,int y){
-        N = arr.size();
-        M = arr[0].size();
-        queue<pair<int, int>> q;
-        vector<pair<int, int>> confirm;
-        int ret = 1;
-        check[x][y] = true;
-        q.push({x, y});
-        while(!q.empty()){
-            int curx = q.front().first;
-            int curt = q.front().second;
-            for(int i = 0;i<4;++i){
-                int nx = curx + dx[i];
-                int ny = cury + dy[i];
-                if(nx < 0 || ny < 0 || nx>=n || ny>=m || arr[curx][cury] >= arr[nx][ny] ||check[nx][ny]) continue;
-                check[nx][ny] = true;
-                q.push({nx, ny});
-            }
-        }
+    int DFS(vector<vector<int>> &arr, vector<vector<int>> &dp, int x, int y, int prev){
+        if(x < 0 || y < 0 || x >= arr.size() || y >= arr[0].size() || arr[x][y] <= prev) return 0;
+        if(dp[x][y]) return dp[x][y];
+        int ret = 0;
+        int left = DFS(arr,dp,x,y - 1,arr[x][y]);
+        int right = DFS(arr,dp,x,y + 1,arr[x][y]);
+        int down = DFS(arr, dp, x + 1,y,arr[x][y]);
+        int up = DFS(arr, dp, x - 1,y,arr[x][y]);
+        ret = max(max(left,right), max(down,up));
+        return dp[x][y] = ret + 1;
     }
     int longestIncreasingPath(vector<vector<int>> &matrix){
-        N = matrix.size();
-        M = matrix[0].size();
-        vector<vector<bool>> check(n, vector<bool>(m, false));
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
         int ans = 1;
         for (int i = 0; i < n; ++i){
-            for(int j=0;j<m;++j){
-                if(check[i][j]) continue;
-                ans = max(ans, BFS(matrix, check, i, j));
+            for(int j = 0; j < m; ++j){
+                int temp = DFS(matrix, dp, i, j, -1);
+                ans = max(ans, temp);
             }
         }
+        return ans;
     }
 };
 
