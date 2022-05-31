54ddf18e699c3ce6f5b9c284fc5bf2f10b9ce1d4
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-06-01 06:07:20 +0900

    Check If a String Contains All Binary Codes of Size K

---
+++ String/1461.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+class Solution{
+public:
+    bool hasAllCodes(string s, int k){
+        if (k > s.size()) return false;
+        unordered_set<string> set;
+        for (int i = 0; i <= s.size() - k; i++){
+            set.insert(s.substr(i, k));
+        }
+        return (set.size() == pow(2, k));
+    }
+};
+int main() {}
