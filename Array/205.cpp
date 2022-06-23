a7350f3c54cc85b9ab0df9f1b12051150c000c61
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-06-23 15:00:28 +0900

    Isomorphic Strings

---
+++ Array/205.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution{
+public:
+    bool isIsomorphic(string s, string t){
+        if (s.size() != t.size()) return false;
+        char map_s[128] = {0,};
+        char map_t[128] = {0,};
+        int length = s.size();
+        for (int i = 0; i < length; i++){
+            if (map_s[s[i]] != map_t[t[i]]) return false;
+            map_s[s[i]] = i + 1;
+            map_t[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
+
+int main() {}
