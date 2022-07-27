4b5bc9a07962ecb7dfed1130ecd8f9c429fbc577
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-07-27 17:23:27 +0900

    Flatten Binary Tree to Linked List

--- Tree/114.cpp
+++ Tree/114.cpp
@@ -9,19 +9,19 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-class Solution{
+class Solution {
 public:
-    TreeNode* Search(TreeNode* root,TreeNode* temp){
-        if(!root) return nullptr;
-        TreeNode *left = Search(root->left);
-        TreeNode *right = Search(root->right);
-        temp->right = left;
-        temp->right = right;
-        return temp;
-    }
-    void flatten(TreeNode *root){
-        TreeNode* temp = Search(root,temp);
-        root = temp;
+    void flatten(TreeNode* root) {//Recursion O(N)
+        if(!root) return;
+        flatten(root->left);
+        flatten(root->right);
+        if(root->left){
+            TreeNode *right = root->right;
+            root->right = root->left;
+            root->left = NULL;
+            while(root->right) root = root->right;
+           root->right = right;
+        }
     }
 };
 
