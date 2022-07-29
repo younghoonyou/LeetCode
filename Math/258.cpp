ba9b5650e2daf7507e852a2633d14244f8dc34bf
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-07-30 08:00:42 +0900

    Add Digits

---
+++ Math/258.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int addDigits(int num) {
+      int sum=0;
+        while(num)
+        {
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10)
+            return sum;
+        else
+            return addDigits(sum);
+    }
+};
+int main(){}
